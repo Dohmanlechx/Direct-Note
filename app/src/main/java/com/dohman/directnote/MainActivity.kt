fe90573418ae721a3d05cd84b2d389a7d0ae414a
Dohmanlechx <dohman_92@hotmail.com>
2019-09-21 13:44:20 +0200

    Created layout. Added listener for the EditText to force up the keyboard.

--- app/src/main/java/com/dohman/directnote/MainActivity.kt
+++ app/src/main/java/com/dohman/directnote/MainActivity.kt
@@ -2,11 +2,24 @@ package com.dohman.directnote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edt_main.post {
+            setupEditTextListener()
+            edt_main.requestFocus()
+        }
+    }
+
+    private fun setupEditTextListener() = edt_main.setOnFocusChangeListener { _, hasFocus ->
+        if (hasFocus) {
+            this.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+        }
     }
 }
